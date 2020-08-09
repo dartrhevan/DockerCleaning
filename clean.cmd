d4fcac0c1f14c6dd2b3fc37433ab4bfe7456a080
dartrhevan <vovasatunkin@mail.ru>
2020-08-09 13:01:37 +0500

    cmd - v 1

--- clean.cmd
+++ clean.cmd
@@ -1,1 +1,11 @@
-for /F %%D in ('docker images --format "{{.ID}}"') do docker rmi %%D
+for /F %%C in ('docker ps -a --format "{{.ID}}"') do docker rm %%C
+
+for /F %%I in ('docker images --format "{{.ID}}"') do (
+	set isExcluded=0
+	for /F %%E in (excluded.txt) do (
+		if %%I==%%E do set isExcluded=1
+	)
+	if isExcluded==1 do docker rmi %%I
+)
+
+set isExcluded=
--- clean.sh
+++ clean.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-excluded=(8ecf5a48c789 ecd67fe340f9 0901fa9da894 38a4d911880c e0373ff33a19)
+#excluded=(8ecf5a48c789 ecd67fe340f9 0901fa9da894 38a4d911880c e0373ff33a19)
 
-for doc in $(docker ps -a --format '{{.ID}}')
+for cont in $(docker ps -a --format '{{.ID}}')
 do 
-	sudo docker rm "$doc"
+	sudo docker rm "$cont"
 done 
 
-for doc in $(docker images --format '{{.ID}}')
+for img in $(docker images --format '{{.ID}}')
 do
 	fl=0
-	for ex in ${excluded[*]}
+	for ex in $(cat 'excluded.txt')#${excluded[*]}
 	do
-		if [ "$ex" = "$doc" ]; then 
+		if [ "$ex" = "$img" ]; then 
 			fl=1
 		fi
 	done	
 	if [ "$fl" -ne 1 ]; then
-		sudo docker rmi "$doc"
+		sudo docker rmi "$img"
 	fi
 done 
 
