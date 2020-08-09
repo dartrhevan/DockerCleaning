1e1dbc1017fe811184d3fcda7b016d5e0a2126d0
dartrhevan <vovasatunkin@mail.ru>
2020-08-09 14:16:24 +0500

    windows version tested

--- clean.cmd
+++ clean.cmd
@@ -1,11 +1,14 @@
 for /F %%C in ('docker ps -a --format "{{.ID}}"') do docker rm %%C
 
+Setlocal EnableDelayedExpansion
 for /F %%I in ('docker images --format "{{.ID}}"') do (
+	setlocal
 	set isExcluded=0
 	for /F %%E in (excluded.txt) do (
-		if %%I==%%E do set isExcluded=1
+		if %%I==%%E set isExcluded=1
 	)
-	if isExcluded==1 do docker rmi %%I
+	if !isExcluded!==0 docker rmi %%I
+	endlocal
 )
 
-set isExcluded=
+rem set isExcluded=
